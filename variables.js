28cc97d23c473a38772494839fb07a51bbe4dea1
Barbie Ordonez <ordonezbarbie@gmail.com>
2022-11-08 13:46:55 -0700

    Show console log statements

--- variables.js
+++ variables.js
@@ -8,3 +8,12 @@ var likeTraveling = true;
 var myAge = 33;
 var numberOfCats = 4;
 var numberOfKids = 4;
+
+var favoriteComedian = "Kristen Wiig";
+var favoriteFood = "lasagna";
+
+console.log(favoriteComedian);
+console.log(myAge);
+console.log(numberOfCats);
+console.log(favoriteFood);
+console.log(numberOfKids);
